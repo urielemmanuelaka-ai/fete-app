e5bede4d2f1265563f36570add607119eea48e81
ASSIELOU <urielemmanuelaka@gmail.com>
2026-06-13 17:59:00 +0000

    Fix production server warning

--- app.py
+++ app.py
@@ -150,4 +150,4 @@ with app.app_context():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+  socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
